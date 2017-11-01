d1458c4fc05e03af731d4c258ffd5852b5ce0340
Lewis Healey <lewis.healey@firebox.com>
2017-11-01 08:48:21 +0000

    Neaten up success page

--- resources/views/user/success.blade.php
+++ resources/views/user/success.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-	<h2>Success!</h2>
-	<p>Thank you for applying to this job.</p>
-	<p>A member of staff will let you know</p>
+
+<div class="container">
+	<div class="row">
+        <div class="col-md-12">
+			<h2>Success!</h2>
+			<p>Thank you for applying to this job.</p>
+			<p>A member of staff will let you know</p>
+		</div>
+	</div>
+</div>
 @endsection
